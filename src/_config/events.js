29684b56538533aad6ec8645815530c03e437eb6
Ben Aultowski <talkingwires@gmail.com>
2025-08-09 13:35:25 -0400

    Fixed truncated filename. How that happened, I could not say…

--- src/_config/events.js
+++ src/_config/events.js
@@ -2,7 +2,7 @@ import {svgToJpeg} from './events/svg-to-jpeg.js';
 import {buildAllCss} from './events/build-css.js';
 import {buildAllJs} from './events/build-js.js';
 import { getLatestCommitInfo } from './events/git-commit-info.js';
-import {buildPagefind} from './events/build-pagefind.js';
+import {buildPagefind} from './events/build-pagefind-index.js';
 
 
 export default {
