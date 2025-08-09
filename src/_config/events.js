5f7cdcca2e72bfe9596a567b509813a6ccb61f26
Ben Aultowski <talkingwires@gmail.com>
2025-08-09 13:28:23 -0400

    Build error from merge 😔

--- src/_config/events.js
+++ src/_config/events.js
@@ -2,12 +2,13 @@ import {svgToJpeg} from './events/svg-to-jpeg.js';
 import {buildAllCss} from './events/build-css.js';
 import {buildAllJs} from './events/build-js.js';
 import { getLatestCommitInfo } from './events/git-commit-info.js';
+import {buildPagefind} from './events/build-pagefind.js';
 
 
 export default {
   svgToJpeg,
   buildAllCss,
   buildAllJs,
-  getLatestCommitInfo
+  getLatestCommitInfo,
+  buildPagefind
 };
-
